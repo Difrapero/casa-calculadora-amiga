cd10b89cb55eeb35a023b3146bc058daea0530ed
Difrapero <dpr2727@gmail.com>
2026-04-06 22:55:01 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
-// 🔴 IMPORTAMOS LAS PÁGINAS LEGALES
+// 🔴 Páginas legales
 import Privacy from "./pages/Privacy.tsx";
 import Cookies from "./pages/cookies.tsx";
 import Legal from "./pages/Legal.tsx";
+
+// 🍪 Banner cookies
+import CookieBanner from "./components/CookieBanner";
 
 const queryClient = new QueryClient();
 
@@ -19,10 +23,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+
+        {/* 🍪 Banner cookies */}
+        <CookieBanner />
+
         <Routes>
           <Route path="/" element={<Index />} />
 
-          {/* 🔴 RUTAS LEGALES */}
+          {/* 🔴 Rutas legales */}
           <Route path="/privacidad" element={<Privacy />} />
           <Route path="/cookies" element={<Cookies />} />
           <Route path="/aviso-legal" element={<Legal />} />
@@ -30,6 +38,7 @@ const App = () => (
           {/* NO TOCAR */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
